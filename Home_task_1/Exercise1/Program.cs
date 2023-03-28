1a978d5089dfeeb18e8f27e1a64251cf417e9103
senivlm <91743906+senivlm@users.noreply.github.com>
2023-03-28 11:45:55 +0300

    Update Program.cs

--- Home_task_1/Exercise1/Program.cs
+++ Home_task_1/Exercise1/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Exercise1;
-
+//Вітаю. Перше завдання по створенню репозиторію Ви виконали.
 SpiralSnakeArray secondSnakeArray = new SpiralSnakeArray(3,4);
 Console.WriteLine(secondSnakeArray.ToString());
